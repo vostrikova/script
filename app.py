5f7f17129c1c5f03449451908a0ce6a3a7f70607
cyber <vostrikova.va@cet-mipt.ru>
2020-09-24 15:38:55 +0300

    WTF

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask
 
+import subprocess
+
+
 app = Flask(__name__)
 
 
 @app.route("/", methods=["POST"])
 def run_script():
-    # RUN SCRIPT
-    pass
-
+    subprocess.Popen("python project_calculation.py", shell=True)
+    return "OK"
 
 
 if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=7878, debug=False)
